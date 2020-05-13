cc0cfa49a45a9dbecf3b0e47230da2d57dc410dc
Mykhailo Dorokhov <mykhailo.dorokhov@pipedrive.com>
2020-05-13 22:35:03 +0300

    CLion project created

---
+++ cmake-build-debug/CMakeFiles/enrollment_system.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/enrollment_system.dir/main.cpp.o"
+  "enrollment_system"
+  "enrollment_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/enrollment_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
